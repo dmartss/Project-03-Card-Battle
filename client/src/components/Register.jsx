869afa075b5206b8495682d5c070253b23a82567
Dan Martin <dmartss13@gmail.com>
2017-12-13 12:44:41 -0500

    register with redux form (still need to dry up field inputs will do next)

--- client/src/components/Register.jsx
+++ client/src/components/Register.jsx
@@ -1,69 +1,37 @@
-import React, { Component } from "react";
-// the new user registration component
+import React from "react";
+import _ from "lodash";
+import { Field, reduxForm } from "redux-form";
+import { connect } from "react-redux";
+import { withRouter } from "react-router-dom";
+import { submitRegister } from "../redux/actions";
 
-class Register extends Component {
-  state = {
-    username: "",
-    password: "",
-    email: "",
-    displayName: ""
-  };
+const Register = ({ handleSubmit, submitRegister, history }) => (
+    <div className="register">
+      <form onSubmit={handleSubmit(submitRegister)}>
+        <div>
+          <label htmlFor="username">First Name</label>
+          <Field name="username" component="input" type="text" />
+        </div>
+        <div>
+          <label htmlFor="password">Last Name</label>
+          <Field name="password" component="input" type="password" />
+        </div>
+        <div>
+          <label htmlFor="email">Email</label>
+          <Field name="email" component="input" type="email" />
+        </div>
+        <div>
+          <label htmlFor="displayName" />
+          <Field name="displayName" component="input" type="text" />
+        </div>
+        <button onClick={() => history.push("/user")} type="submit">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+};
 
-  handleInputChange = e => {
-    const name = e.target.name;
-    const value = e.target.value;
-    this.setState({
-      [name]: value
-    });
-  };
-
-  render() {
-    const { username, password, email, displayName } = this.state;
-    return (
-      <div className="register">
-        <form
-          onSubmit={e =>
-            this.props.handleRegisterSubmit(
-              e,
-              username,
-              password,
-              email,
-              displayName
-            )}
-        >
-          <input
-            type="text"
-            name="username"
-            value={username}
-            placeholder="Username"
-            onChange={this.handleInputChange}
-          />
-          <input
-            type="password"
-            name="password"
-            value={password}
-            placeholder="Password"
-            onChange={this.handleInputChange}
-          />
-          <input
-            type="email"
-            name="email"
-            value={email}
-            placeholder="email"
-            onChange={this.handleInputChange}
-          />
-          <input
-            type="text"
-            name="displayName"
-            value={displayName}
-            placeholder="Name"
-            onChange={this.handleInputChange}
-          />
-          <button type="submit">Register</button>
-        </form>
-      </div>
-    );
-  }
-}
-
-export default Register;
+export default reduxForm({ form: "registerForm" })(
+  connect(null, { submitRegister })(withRouter(Register))
+);
