a5e05296250ec527f9653459a4a53d0bec821404
Dan Martin <dmartss13@gmail.com>
2017-12-13 12:45:54 -0500

    typo

--- client/src/components/Register.jsx
+++ client/src/components/Register.jsx
@@ -6,31 +6,30 @@ import { withRouter } from "react-router-dom";
 import { submitRegister } from "../redux/actions";
 
 const Register = ({ handleSubmit, submitRegister, history }) => (
-    <div className="register">
-      <form onSubmit={handleSubmit(submitRegister)}>
-        <div>
-          <label htmlFor="username">First Name</label>
-          <Field name="username" component="input" type="text" />
-        </div>
-        <div>
-          <label htmlFor="password">Last Name</label>
-          <Field name="password" component="input" type="password" />
-        </div>
-        <div>
-          <label htmlFor="email">Email</label>
-          <Field name="email" component="input" type="email" />
-        </div>
-        <div>
-          <label htmlFor="displayName" />
-          <Field name="displayName" component="input" type="text" />
-        </div>
-        <button onClick={() => history.push("/user")} type="submit">
-          Submit
-        </button>
-      </form>
-    </div>
-  );
-};
+  <div className="register">
+    <form onSubmit={handleSubmit(submitRegister)}>
+      <div>
+        <label htmlFor="username">First Name</label>
+        <Field name="username" component="input" type="text" />
+      </div>
+      <div>
+        <label htmlFor="password">Last Name</label>
+        <Field name="password" component="input" type="password" />
+      </div>
+      <div>
+        <label htmlFor="email">Email</label>
+        <Field name="email" component="input" type="email" />
+      </div>
+      <div>
+        <label htmlFor="displayName" />
+        <Field name="displayName" component="input" type="text" />
+      </div>
+      <button onClick={() => history.push("/user")} type="submit">
+        Submit
+      </button>
+    </form>
+  </div>
+);
 
 export default reduxForm({ form: "registerForm" })(
   connect(null, { submitRegister })(withRouter(Register))
