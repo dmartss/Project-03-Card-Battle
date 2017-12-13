afe84ac9743bcfc903e9aaa4eb697fb60c8b1b59
Dan Martin <dmartss13@gmail.com>
2017-12-13 12:15:37 -0500

    reducers for registering

--- client/src/redux/reducers/index.js
+++ client/src/redux/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from "redux";
 import { reducer as formReducer } from "redux-form";
+import auth from "./reducer_auth";
 
 export default combineReducers({
-  form: formReducer
+  form: formReducer,
+  auth
 });
