76d4b456ed20a66517e752fe5fb3c7a4926c80bb
Dan Martin <dmartss13@gmail.com>
2017-12-21 16:33:49 -0500

    single card destructure

--- client/src/components/HandCardSingle.jsx
+++ client/src/components/HandCardSingle.jsx
@@ -2,20 +2,22 @@ import React from "react";
 
 // special card component for the user playing cards,
 // contains click listener for user to select card ONLY IF THERE IS AN OPPONENT
-export const HandCardSingle = ({ card, opponent, select }) => (
-  <div
-    className={`card ${card.class}`}
-    onClick={opponent && (() => select(card))}
-  >
+export const HandCardSingle = ({
+  card: { name, attack, defense, type },
+  card,
+  opponent,
+  select
+}) => (
+  <div className={`card ${type}`} onClick={opponent && (() => select(card))}>
     <div className="card-top">
       <div className="card-name">
-        <b>{card.name}</b>
-        <p>{card.class}</p>
+        <b>{name}</b>
+        <p>{type}</p>
       </div>
     </div>
     <div className="card-numbers">
-      <p>{card.attack && `ATT ${card.attack}`}</p>
-      <p>{card.defense && `DEF ${card.defense}`}</p>
+      <p>{attack && `ATT ${attack}`}</p>
+      <p>{defense && `DEF ${defense}`}</p>
     </div>
   </div>
 );
