a8b4c27ea483fd4407a23c9b6db3100ab582e622
Dan Martin <dmartss13@gmail.com>
2018-01-11 15:25:43 -0500

    push history in action creator

--- client/src/redux/actions/index.js
+++ client/src/redux/actions/index.js
@@ -1,10 +1,11 @@
 import axios from "axios";
 export const REGISTER_SUBMIT = "REGISTER_SUBMIT";
 
-export const submitRegister = values => async dispatch => {
+export const submitRegister = (values, history) => async dispatch => {
   try {
+    console.log(values);
     const res = await axios.post("/auth/register", values);
-
+    history.push("/user");
     dispatch({ type: REGISTER_SUBMIT, payload: res.data });
   } catch (err) {
     console.log(err);
