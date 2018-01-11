05619191ef5906613b01291597323864a5f0e221
Dan Martin <dmartss13@gmail.com>
2018-01-11 15:26:02 -0500

    register fields array

---
+++ client/src/components/regFields.js
@@ -0,0 +1,6 @@
+export default [
+  { placeholder: "username", type: "text", name: "username" },
+  { placeholder: "password", type: "password", name: "password" },
+  { placeholder: "email", type: "email", name: "email" },
+  { placeholder: "displayName", type: "text", name: "displayName" }
+];
