18630caea8b73a882bb41d4324263d90715efc2f
Dan Martin <dmartss13@gmail.com>
2018-01-11 15:26:33 -0500

    register component with improved redux form

--- client/src/components/Register.jsx
+++ client/src/components/Register.jsx
@@ -4,33 +4,31 @@ import { Field, reduxForm } from "redux-form";
 import { connect } from "react-redux";
 import { withRouter } from "react-router-dom";
 import { submitRegister } from "../redux/actions";
+import registerFields from "./regFields";
+import registerRender from "./registerRender";
 
-const Register = ({ handleSubmit, submitRegister, history }) => (
-  <div className="register">
-    <form onSubmit={handleSubmit(submitRegister)}>
-      <div>
-        <label htmlFor="username">First Name</label>
-        <Field name="username" component="input" type="text" />
-      </div>
-      <div>
-        <label htmlFor="password">Last Name</label>
-        <Field name="password" component="input" type="password" />
-      </div>
-      <div>
-        <label htmlFor="email">Email</label>
-        <Field name="email" component="input" type="email" />
-      </div>
-      <div>
-        <label htmlFor="displayName" />
-        <Field name="displayName" component="input" type="text" />
-      </div>
-      <button onClick={() => history.push("/user")} type="submit">
-        Submit
-      </button>
-    </form>
-  </div>
-);
+const Register = ({ handleSubmit, submitRegister, history }) => {
+  const submitAndRedirect = (values, history) =>
+    submitRegister(values, history);
+  const fieldsNow = _.map(registerFields, ({ type, placeholder, name }) => (
+    <Field
+      key={name}
+      component={registerRender}
+      placeholder={placeholder}
+      type={type}
+      name={name}
+    />
+  ));
+  return (
+    <div className="register">
+      <form id="register" onSubmit={handleSubmit(submitAndRedirect)}>
+        {fieldsNow}
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+};
 
-export default reduxForm({ form: "registerForm" })(
+export default reduxForm({ form: "register" })(
   connect(null, { submitRegister })(withRouter(Register))
 );
