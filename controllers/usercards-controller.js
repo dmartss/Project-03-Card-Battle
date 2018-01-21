632f7295037d0db3aa39f73fffd83ed06bf8a5cc
Dan Martin <dmartss13@gmail.com>
2018-01-21 10:45:49 -0500

    dry user card controller

--- controllers/usercards-controller.js
+++ controllers/usercards-controller.js
@@ -1,74 +1,80 @@
-const Usercard = require("../models/usercard");
+const {
+  findUserCards,
+  addToUser,
+  update,
+  destroy,
+  findFiveUserCards
+} = require("../models/usercard");
 
-const usercardsController = {};
-//show users' cards, user cards page
-usercardsController.findUserCards = async (req, res) => {
-  try {
-    const usercards = await Usercard.findUserCards(req.user.id);
-    return res.json(usercards);
-  } catch (err) {
-    console.log(err);
-    res.status(500).json(err);
+module.exports = usercardsController = {
+  //show users' cards, user cards page
+  findUserCards: async (req, res) => {
+    try {
+      const usercards = await findUserCards(req.user.id);
+      return res.json(usercards);
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+    }
+  },
+  //add card to users_cards
+  addToUser: async (req, res) => {
+    try {
+      const { cardId, name, type, attack, defense, imageUrl } = req.body;
+      const usercard = await addToUser(
+        {
+          cardId,
+          name,
+          type,
+          attack,
+          defense,
+          imageUrl
+        },
+        req.user.id
+      );
+      return res.json(usercard);
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+    }
+  },
+  //edit users' card info
+  updateCard: async (req, res) => {
+    try {
+      const { params: { id }, body: { name } } = req;
+      const usercard = await console.log(id);
+      await update(name, id);
+      return res.json(usercard);
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+    }
+  },
+  //delete one card
+  deleteCard: async (req, res) => {
+    try {
+      const usercard = await destroy(req.params.id);
+      return res.json({
+        message: "ok",
+        data: usercard
+      });
+    } catch (err) {
+      console.log(err);
+      res.status(500).json({ err });
+    }
+  },
+  //find random five user cards, for battle ready page
+  findFiveUserCards: async (req, res) => {
+    try {
+      const UserCard = await findFiveUserCards(req.user.id);
+      await UserCard.findFiveUserCards(1);
+      return {
+        userCard,
+        opponentCard
+      };
+      return res.json(data);
+    } catch (err) {
+      console.log(err);
+    }
   }
 };
-//add card to users_cards
-usercardsController.addToUser = async (req, res) => {
-  try {
-    const { cardId, name, type, attack, defense, imageUrl } = req.body;
-    const usercard = await Usercard.addToUser(
-      {
-        cardId,
-        name,
-        type,
-        attack,
-        defense,
-        imageUrl
-      },
-      req.user.id
-    );
-    return res.json(usercard);
-  } catch (err) {
-    console.log(err);
-    res.status(500).json(err);
-  }
-};
-//edit users' card info
-usercardsController.update = async (req, res) => {
-  try {
-    const usercard = await console.log(req.params);
-    await Usercard.update(req.body.name, req.params.id);
-    return res.json(usercard);
-  } catch (err) {
-    console.log(err);
-    res.status(500).json(err);
-  }
-};
-//delete one card
-usercardsController.delete = async (req, res) => {
-  try {
-    const usercard = await Usercard.destroy(req.params.id);
-    return res.json({
-      message: "ok",
-      data: usercard
-    });
-  } catch (err) {
-    console.log(err);
-    res.status(500).json({ err });
-  }
-};
-//find random five user cards, for battle ready page
-usercardsController.findFiveUserCards = async (req, res) => {
-  try {
-    const UserCard = await Usercard.findFiveUserCards(req.user.id);
-    await Usercard.findFiveUserCards(1);
-    return {
-      userCard,
-      opponentCard
-    };
-    return res.json(data);
-  } catch (err) {
-    console.log(err);
-  }
-};
-
-module.exports = usercardsController;
