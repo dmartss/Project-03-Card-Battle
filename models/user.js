ac7db435c890ccdf40c2e523098419dcb1f9f155
Dan Martin <dmartss13@gmail.com>
2018-01-21 10:45:39 -0500

    destructure user model

--- models/user.js
+++ models/user.js
@@ -1,64 +1,64 @@
-const db = require("../db/config");
-const User = {};
-//find users' cards
-User.findByUserName = username =>
-  db.oneOrNone(
-    `
+const { findByUserName, oneOrNone, one, query, none } = require("../db/config");
+
+module.exports = User = {
+  //find users' cards
+  findByUserName: username =>
+    oneOrNone(
+      `
     SELECT * FROM users
     WHERE username = $1
     `,
-    [username]
-  );
-//create new users
-User.create = user =>
-  db.one(
-    `
+      [username]
+    ),
+  //create new users
+  create: ({ username, password_digest, displayName, email }) =>
+    one(
+      `
     INSERT INTO users
     (username, password_digest, display_name, email, wins, currency)
     VALUES ($1, $2, $3, $4, 0, 100)
     RETURNING *
     `,
-    [user.username, user.password_digest, user.displayName, user.email]
-  );
-//edit users info
-User.update = (displayName, email, id) =>
-  db.one(
-    `
+      [username, password_digest, displayName, email]
+    ),
+  //edit users info
+  update: (displayName, email, id) =>
+    one(
+      `
   UPDATE users SET
   display_name = $1,
   email = $2
   WHERE id = $3
   RETURNING *
   `,
-    [displayName, email, id]
-  );
-//leader board page, show top ten users according to their wins
-User.showLeaderboard = () =>
-  db.query(`
+      [displayName, email, id]
+    ),
+  //leader board page, show top ten users according to their wins
+  showLeaderboard: () =>
+    query(`
     SELECT * FROM users
     ORDER BY wins DESC
     LIMIT 10
-    `);
-//delete usrs
-User.destroy = (id, user_id) =>
-  db.none(
-    `
+    `),
+  //delete usrs
+  destroy: (id, user_id) =>
+    none(
+      `
   DELETE FROM users_cards where user_id = $1;
   DELETE FROM users WHERE id = $1
   `,
-    [id]
-  );
-//update currency and wins after winning
-User.updateCurrencyNWins = user =>
-  db.one(
-    `
+      [id]
+    ),
+  //update currency and wins after winning
+  updateCurrencyNWins: ({ wins, currency, username }) =>
+    one(
+      `
     UPDATE users SET
     wins = $1,
     currency = $2
     WHERE username = $3
     RETURNING *
     `,
-    [user.wins, user.currency, user.username]
-  );
-
-module.exports = User;
+      [wins, currency, username]
+    )
+};
