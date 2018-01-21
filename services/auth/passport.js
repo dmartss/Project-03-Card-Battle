95f7b9fda62c2b71610190d60b06126213ca9100
Dan Martin <dmartss13@gmail.com>
2018-01-21 10:43:33 -0500

    destructure passport

--- services/auth/passport.js
+++ services/auth/passport.js
@@ -1,17 +1,18 @@
-const passport = require('passport');
-const User = require('../../models/user');
+const passport = require("passport");
+const { findByUserName } = require("../../models/user");
 
 module.exports = () => {
-    passport.serializeUser((user, done) => {
-        done(null, user.username);
-    });
+  passport.serializeUser((user, done) => {
+    done(null, user.username);
+  });
 
-    passport.deserializeUser((username, done) => {
-        User.findByUserName(username)
-        .then(user => {
-            done(null, user);
-        }).catch(err => {
-            done(err, null);
-        });
-    });
+  passport.deserializeUser((username, done) => {
+    findByUserName(username)
+      .then(user => {
+        done(null, user);
+      })
+      .catch(err => {
+        done(err, null);
+      });
+  });
 };
