e9348d4afa4a7057172f8dc3f377b411ed01072c
Dan Martin <dmartss13@gmail.com>
2018-01-25 10:13:43 -0500

    create global theme

---
+++ client/src/theme/themeProvider.js
@@ -0,0 +1,39 @@
+const theme = {};
+
+theme.palette = {
+  background: ['#e8ddaf', '#12183b'],
+  primary: ['#12183b'],
+  primaryBackground: ['#9ca7d5'],
+  button: ['#fbf6e0', '#dedede', '#485095', '#ffffff', '#bdbdbd', '#303e63'],
+  footer: ['#12183b', '#faf5e0'],
+  selected: ['#cbd2ea'],
+  appHeader: ['#222'],
+  user: ['#fdf6cf', '#12183b', '#484f94'],
+  cards: ['#484f94', '#606d7c', '#833b3c', '#4c5e47', '#bd4e4e', '#ca9a2c']
+};
+
+theme.fonts = {
+  primary: 'Helvetica Neue, Helvetica, Roboto, sans-serif',
+  oxygen: 'Oxygen, sans-serif',
+  main: 'UnifrakturCook, cursive',
+  li: 'baskerville',
+  headerLi: 'Yanone Kaffeesatz, sans-serif'
+};
+
+export default theme;
+
+export const background = '#e8ddaf';
+export const primary = '#12183b';
+export const background2 = '#485095';
+export const button = '#fbf6e0';
+export const button2 = '#bdbdbd';
+export const button3 = '#303e63';
+export const footer = '#12183b';
+export const footer1 = '#faf5e0';
+
+export const mainFont = 'UnifrakturCook, cursive';
+export const headerFont = 'Yanone Kaffeesatz, sans-serif';
+export const oxygen = 'Oxygen, sans-serif';
+
+export const regFontSize = '16px';
+export const biggerFontSize = 'Oxygen, sans-serif';
