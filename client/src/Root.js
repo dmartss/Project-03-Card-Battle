ce078c8754540f52419edbacd62c1d274198ff9c
Dan Martin <dmartss13@gmail.com>
2018-01-25 10:14:07 -0500

    import theme provider for whole app

--- client/src/Root.js
+++ client/src/Root.js
@@ -1,12 +1,16 @@
-import React from "react";
-import { Provider } from "react-redux";
-import App from "./App";
-import { BrowserRouter } from "react-router-dom";
+import React from 'react';
+import { Provider } from 'react-redux';
+import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
+import theme from './theme/themeProvider';
 
 export default ({ store }) => (
   <Provider store={store}>
     <BrowserRouter>
-      <App />
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
     </BrowserRouter>
   </Provider>
 );
