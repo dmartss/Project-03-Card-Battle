97030a3702e0e3f1c30f3b82ba01beacd9805f3f
Dan Martin <dmartss13@gmail.com>
2018-01-25 10:14:27 -0500

    remove styles moved to inject global

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import * as firebase from 'firebase';
-
 import { Route, Switch, withRouter } from 'react-router-dom';
 
 import './App.css';
--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,9 @@
-import React from "react";
-import { render } from "react-dom";
-import "./index.css";
-import { configureStore } from "./configureStore";
-import Root from "./Root";
+import React from 'react';
+import { render } from 'react-dom';
+import { configureStore } from './configureStore';
+import Root from './Root';
+import './theme/global';
 
 const store = configureStore();
 
-render(<Root store={store} />, document.querySelector("#root"));
+render(<Root store={store} />, document.querySelector('#root'));
